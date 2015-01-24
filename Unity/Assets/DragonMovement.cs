3d6f36d708f70339e926ae848009e7232929a9aa
Markus Ljung <markus.ljung@gmail.com>
2015-01-24 21:01:19 +0100

    add fog, treasure box and dragon with consummate V's

---
+++ Unity/Assets/DragonMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddRelativeTorque (Vector3.down * 7);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody.AddRelativeForce (new Vector3(0f, 0f, 1f) * 400);
+	}
+}
